a010073607b0b6f47b9391fd3966dcbd1b9477ab
laixintao <nanshi.lxt@alipay.com>
2018-08-10 20:05:14 +0800

    commit code

---
+++ for_loop.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    sum := 10
+    for i:=0; i < 10; i++ {
+        sum += 1
+    }
+    fmt.Println(sum)
+}
---
+++ temp.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+
+func main() {
+    a := 10
+    fmt.Println(a)
+}
